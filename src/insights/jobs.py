5f81669c94eebef373783f5d3336ee184131425b
Lucas da Silva <lucassouza00012345@gmail.com>
2023-03-24 17:46:55 -0300

    feat: implements get_unique_job_types function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -16,21 +16,13 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    jobs = read(path)
+    jobs_type = set()
 
-    Must call `read`
+    for job in jobs:
+        jobs_type.add(job["job_type"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    return jobs_type
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
