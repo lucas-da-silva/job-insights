8e98c547ac7bc0479a8ad0b91aa9bba61edd3dc0
Lucas da Silva <lucassouza00012345@gmail.com>
2023-03-24 19:03:31 -0300

    refactor: remove unnecessary for in get_unique_industries function

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -4,13 +4,7 @@ from src.insights.jobs import read
 
 def get_unique_industries(path: str) -> List[str]:
     jobs = read(path)
-    unique_industries = set()
-
-    for job in jobs:
-        if job["industry"]:
-            unique_industries.add(job["industry"])
-
-    return unique_industries
+    return {job["industry"] for job in jobs if job["industry"]}
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
