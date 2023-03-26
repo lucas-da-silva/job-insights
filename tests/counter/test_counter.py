8b4eb964d9be50cf3803c6bcdac9c1e3672d40cd
Lucas da Silva <lucassouza00012345@gmail.com>
2023-03-26 12:01:40 -0300

    test: create tests for count_ocurrences function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,20 @@
-# from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+from src.pre_built.counter import count_ocurrences
+
+PATH = "data/jobs.csv"
+WORDS = ["Javascript", "Python"]
+COUNT_WORD = [2, 3]
 
 
 def test_counter():
-    pass
+    jobs = """Python, 
+    Java, 
+    JavaScript, 
+    HTML, CSS, 
+    React, Redux, Python, 
+    JavaScript, Python
+    """
+
+    with patch("builtins.open", mock_open(read_data=jobs)):
+        for index, word in enumerate(WORDS):
+            assert count_ocurrences(PATH, word) == COUNT_WORD[index]
