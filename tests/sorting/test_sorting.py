6c2263264e59b4e9463156b01fa718b6f13951e6
Lucas da Silva <lucassouza00012345@gmail.com>
2023-03-27 10:41:20 -0300

    test: create tests and mocks for sort_by function

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,18 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from tests.mocks.jobs_sorting import (
+    jobs,
+    jobs_sorted_date_posted,
+    jobs_sorted_max_salary,
+    jobs_sorted_min_salary,
+)
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "min_salary")
+    assert jobs == jobs_sorted_min_salary
+
+    sort_by(jobs, "max_salary")
+    assert jobs == jobs_sorted_max_salary
+
+    sort_by(jobs, "date_posted")
+    assert jobs == jobs_sorted_date_posted
